20d8be1ccb95fc030313795d57360fb9a3383633
meleryz <meleryz@gmail.com>
2020-06-30 16:20:43 +0300

    Add expiration time tracker

--- lib/live_learn_web/live/license_live.ex
+++ lib/live_learn_web/live/license_live.ex
@@ -1,64 +1,60 @@
-defmodule LiveLearnWeb.LicenseLive do
-  use LiveLearnWeb, :live_view
+defmodule LiveViewStudioWeb.LicenseLive do
+  use LiveViewStudioWeb, :live_view
 
-  alias LiveLearn.Licenses
-
+  alias LiveViewStudio.Licenses
   import Number.Currency
-
-  @default_seats 2
-  @default_expire 1
+  import Timex
 
   def mount(_params, _session, socket) do
     if connected?(socket) do
       :timer.send_interval(1000, self(), :tick)
     end
 
-    expiration_time = Timex.shift(Timex.now(), hours: @default_expire)
-    # socket = assign(socket, seats: @default_seats, amount: Licenses.calculate(2))
+    expiration_time = Timex.shift(Timex.now(), hours: 1)
+
     socket =
       assign(socket,
-        seats: @default_seats,
-        expiration_time: expiration_time,
-        time_remaining: time_remaining(expiration_time),
-        amount: Licenses.calculate(@default_seats)
-      )
+            seats: 2,
+            amount: Licenses.calculate(3),
+            expiration_time: expiration_time,
+            time_remaining: time_remaining(expiration_time)
+            )
 
     {:ok, socket}
   end
 
   def render(assigns) do
-      ~L"""
-      <h1>Team License</h1>
-      <div id="license">
-        <div class="card">
-          <div class="content">
-            <div class="seats">
-              <img src="images/license.svg">
-              <span>
-                Your license is currently for
-                <strong><%= @seats %></strong> seats.
-              </span>
-            </div>
+    ~L"""
+    <h1>Team License</h1>
+    <div id="license">
+      <div class="card">
+        <div class="content">
 
-            <form phx-change="update">
-              <input type="range" min="1" max="10"
-                    name="seats" value="<%= @seats %>" />
-            </form>
+        <p class="m-4 font-semibold text-indigo-800">
+          <%= @time_remaining %> left to save 20%
+        </p>
 
-            <div class="amount">
-              <%= number_to_currency(@amount) %>
-            </div>
+          <div class="seats">
+            <img src="images/license.svg">
+            <span>
+              Your license is currently for
+              <strong><%= @seats %></strong> seats.
+            </span>
           </div>
 
-          <p class="m-4 font-semibold text-indigo-800">
-            <%= @time_remaining %> left to save 20%
-          </p>
+          <form phx-change="update">
+            <input type="range" min="1" max="10"
+                  name="seats" value="<%= @seats %>" />
+          </form>
 
+          <div class="amount">
+            <%= number_to_currency(@amount) %>
+          </div>
         </div>
       </div>
-      """
+    </div>
+    """
   end
-
 
   def handle_event("update", %{"seats" => seats}, socket) do
     seats = String.to_integer(seats)
@@ -72,13 +68,17 @@ defmodule LiveLearnWeb.LicenseLive do
     {:noreply, socket}
   end
 
-
   def handle_info(:tick, socket) do
-    expiration_time = socket.assigns.expiration_time
-    socket = assign(socket, time_remaining: time_remaining(expiration_time))
+    exp_time = socket.assigns.expiration_time
+
+    socket = assign(socket,
+                    time_remaining: time_remaining(exp_time)
+                  )
+
     {:noreply, socket}
   end
 
+  ### Private funcs ###
 
   defp time_remaining(expiration_time) do
     Timex.Interval.new(from: Timex.now(), until: expiration_time)
